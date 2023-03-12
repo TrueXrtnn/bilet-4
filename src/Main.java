fd4dc6e4bdd35578e2ea7928272fb6da75362c20
true.xrstn <mr.chareev@mail.ru>
2023-03-13 04:32:11 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] array = {"привет", "меня", "меня", "привет", "зовут", "Илья", "тебя", "зовут", "Илья", "!"};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null)
+                for (int i1 = 0; i1 < array.length; i1++) {
+                    if (i != i1 && array[i1] != null && array[i].equals(array[i1])) {
+                        array[i1] = null;
+                    }
+
+                }
+        }
+        String stroka = "";
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                stroka = stroka + array[i];
+            }
+        }
+        System.out.println(stroka);
+
     }
+
 }
